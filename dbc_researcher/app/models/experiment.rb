be68942b6ae0b36184089b521e5daa3f48dd1b45
Kyle Dolezal <email": null>
2015-11-11 17:21:39 -0600

    Setup associations

--- dbc_researcher/app/models/experiment.rb
+++ dbc_researcher/app/models/experiment.rb
@@ -1,2 +1,9 @@
 class Experiment < ActiveRecord::Base
+  belongs_to :proposal
+  belongs_to :cohort
+  has_many :observations
+
+  has_many :comments, as: :commentable
+  has_many :proposals, through: :commentable
+
 end
