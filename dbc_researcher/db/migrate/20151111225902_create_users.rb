0ef490cbce79b995ad7af88cc70d3e935a920d76
Lorena <email": null>
2015-11-11 17:02:00 -0600

    add constraints to user migration

--- dbc_researcher/db/migrate/20151111225902_create_users.rb
+++ dbc_researcher/db/migrate/20151111225902_create_users.rb
@@ -1,11 +1,11 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :name
-      t.string :title
-      t.string :email
+      t.string :name, null: false
+      t.string :title, null: false
+      t.string :email, null: false
       t.string :password_digest
-      t.string :admin
+      t.boolean :admin, {default: false}
 
       t.timestamps null: false
     end
