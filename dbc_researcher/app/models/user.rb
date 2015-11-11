dc885fba83a4e6150cc2a65964fb10b9e2a4fe17
Lorena <email": null>
2015-11-11 17:17:24 -0600

    user validations all work now

--- dbc_researcher/app/models/user.rb
+++ dbc_researcher/app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
-  validates :name :title :password_digest :email, presence: true
+  validates :name, :title, :password_digest, :email, presence: true
   validates :email, uniqueness: true
-  validates_format_if :email, :with => /.*@devbootcamp.com/, :on => :create
+  validates_format_of :email, :with => /.*@devbootcamp.com/, :on => :create
 
 end
