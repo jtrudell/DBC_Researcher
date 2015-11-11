0df215801f5b72337aa8142aa04cf11387227e02
Kyle Dolezal <email": null>
2015-11-11 17:33:30 -0600

    Table is set up correctly

--- dbc_researcher/app/models/experiment.rb
+++ dbc_researcher/app/models/experiment.rb
@@ -2,8 +2,11 @@ class Experiment < ActiveRecord::Base
   belongs_to :proposal
   belongs_to :cohort
   has_many :observations
-
   has_many :comments, as: :commentable
   has_many :proposals, through: :commentable
+
+  validates :proposal_id, presence: true
+  validates :cohort_id, presence: true
+  validates :cohort_id, presence: true
 
 end
