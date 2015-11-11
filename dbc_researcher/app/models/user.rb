6ef39b079596d1c96963406acb13a21917d3cd3c
Lorena <email": null>
2015-11-11 17:07:53 -0600

    validates email address is a dbc email

--- dbc_researcher/app/models/user.rb
+++ dbc_researcher/app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ActiveRecord::Base
+  validates :name :title :password_digest :email, presence: true
+  validates :email, uniqueness: true
+  validates_format_if :email, :with => /.*@devbootcamp.com/, :on => :create
+
 end
