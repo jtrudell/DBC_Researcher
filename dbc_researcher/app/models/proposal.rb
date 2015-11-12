5d44706fa86cfd03b04e7534999cd3e7f6a53d07
Lorena <email": null>
2015-11-12 10:15:28 -0600

    add proposal validations and constraints

--- dbc_researcher/app/models/proposal.rb
+++ dbc_researcher/app/models/proposal.rb
@@ -1,5 +1,8 @@
 class Proposal < ActiveRecord::Base
   belongs_to :user
+
   has_many :experiments
   has_many :comments, as: :commentable
+
+  validates :title, :problem_description, :hypothesis, :user_id, presence: true
 end
