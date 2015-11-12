532a0666829aed742a95fb9d547731784b797315
Lorena <email": null>
2015-11-12 10:25:03 -0600

    add controller and routes for observations

---
+++ dbc_researcher/app/controllers/observations_controller.rb
@@ -0,0 +1,13 @@
+class ObservationsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def destroy
+  end
+
+  def create
+  end
+end
--- dbc_researcher/config/routes.rb
+++ dbc_researcher/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+    resources :observations
     resources :proposals
     resources :experiments
 
