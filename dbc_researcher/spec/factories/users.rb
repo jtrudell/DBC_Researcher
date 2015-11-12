e44ec86fb41472856079809a10ebab85eb55f192
Jeanette Trudell <jtrudell@gmail.com>
2015-11-12 13:50:15 -0600

    add user validation tests

--- dbc_researcher/spec/factories/users.rb
+++ dbc_researcher/spec/factories/users.rb
@@ -1,6 +1,31 @@
 FactoryGirl.define do
   factory :user do
-    
+    name "Stefanie"
+    title "Teacher"
+    email "stef@devbootcamp.com"
+    password_digest "password"
+  end
+
+  trait :admin do
+    name "Joe"
+    title "Faculty"
+    email "joe@devbootcamp.com"
+    password_digest "password"
+    admin true
+  end
+
+  trait :student do
+    name "Bob"
+    title "Student"
+    email "bob@bob.com"
+    password_digest "password"
+  end
+
+  trait :duplicate do
+    name "Jill"
+    title "Faculty"
+    email "stef@devbootcamp.com"
+    password_digest "password"
   end
 
 end
--- dbc_researcher/spec/models/user_spec.rb
+++ dbc_researcher/spec/models/user_spec.rb
@@ -1,5 +1,44 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+# RSpec.describe User, type: :model do
+#   pending "add some examples to (or delete) #{__FILE__}"
+# end
+
+describe User do
+  it 'has a valid factory' do
+    expect(build(:user)).to be_valid
+  end
+
+  it 'is invalid without a name' do
+    expect(build(:user, name: nil)).to_not be_valid
+  end
+
+  it 'is invalid without an email' do
+    expect(build(:user, email: nil)).to_not be_valid
+  end
+
+  it 'is invalid without a title' do
+    expect(build(:user, title: nil)).to_not be_valid
+  end
+
+  it 'is invalid without a password' do
+    expect(build(:user, password_digest: nil)).to_not be_valid
+  end
+
+  it 'is invalid without a dbc email address' do
+    expect(build(:user, :student)).to_not be_valid
+  end
+
+  it 'can create an admin user with the admin property true' do
+    expect(build(:user, :admin).admin).to eq true
+  end
+
+  it 'creates users by default with a false admin property' do
+    expect(build(:user).admin).to eq false
+  end
+
+  it 'will not create a user that has an email identical to another user' do
+    create(:user)
+    expect(build(:user, :duplicate)).to_not be_valid
+  end
 end
