d7fbb5bfec3f38af0dabffe316f0ffe9d7c06792
Jeanette Trudell <jtrudell@gmail.com>
2015-11-12 14:06:10 -0600

    add tests for proposal model

--- dbc_researcher/spec/models/user_spec.rb
+++ dbc_researcher/spec/models/user_spec.rb
@@ -1,9 +1,5 @@
 require 'rails_helper'
 
-# RSpec.describe User, type: :model do
-#   pending "add some examples to (or delete) #{__FILE__}"
-# end
-
 describe User do
   it 'has a valid factory' do
     expect(build(:user)).to be_valid
