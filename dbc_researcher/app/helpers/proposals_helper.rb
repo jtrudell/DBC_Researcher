ce14c2fe0639b0e0a3eed99312a12eae4f2fbe60
Jeanette Trudell <jtrudell@gmail.com>
2015-11-12 16:31:02 -0600

    add side bar for open proposals

--- dbc_researcher/app/helpers/proposals_helper.rb
+++ dbc_researcher/app/helpers/proposals_helper.rb
@@ -3,4 +3,8 @@ module ProposalsHelper
   def approved_status(proposal)
     proposal.approved ? "Approved" : "Open"
   end
+
+  def pending_proposals
+    Proposal.all.select { |proposal| proposal.approved != true }
+  end
 end
