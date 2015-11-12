3c36e0969e180b3fdc0fb53c824c1bea99cf5e22
Kyle Dolezal <email": null>
2015-11-12 14:43:35 -0600

    Handle proper experiment creation and errors

--- dbc_researcher/app/controllers/experiments_controller.rb
+++ dbc_researcher/app/controllers/experiments_controller.rb
@@ -13,9 +13,12 @@ class ExperimentsController < ApplicationController
 
       experiment = Experiment.new(proposal_id: params[:proposal_id], cohort_id: cohort_params[:cohort], user_id: session[:user_id], experiment_description: exp_params[:experiment_description], required_supplies_for_experiment: exp_params[:required_supplies_for_experiment], goal_description: exp_params[:goal_description], conclusions: exp_params[:conclusions])
 
-      experiment.save
-
-    redirect_to proposal_experiments_path
+      if experiment.save
+        redirect_to proposal_experiments_path
+      else
+        flash[:error] = "Error, please enter valid experiment information."
+        redirect_to new_proposal_experiment_path(params[:proposal_id])
+      end
   end
   def exp_params
       params.require(:experiment).permit(:required_supplies_for_experiment, :goal_description, :conclusions, :experiment_description)
