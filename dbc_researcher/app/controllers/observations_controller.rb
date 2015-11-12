74cdea7723b5c47dee1b8d135577b97b45be8715
Lorena <email": null>
2015-11-12 10:48:33 -0600

    add observation methods and forms

--- dbc_researcher/app/controllers/observations_controller.rb
+++ dbc_researcher/app/controllers/observations_controller.rb
@@ -1,13 +1,23 @@
 class ObservationsController < ApplicationController
-  def index
+  def show
   end
 
-  def show
+  def new
+    @observation = Observation.new
+  end
+
+  def edit
+    @observation = Observation.find_by(id: params[:id])
   end
 
   def destroy
   end
 
   def create
+    if @observation && @observation.save
+      redirect_to '/'
+    else
+      redirect_to '/observations/new'
+    end
   end
 end
