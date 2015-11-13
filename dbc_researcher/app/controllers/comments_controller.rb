9c70fc8f12b29726883f1fc06c668d52d0153d86
Lorena <email": null>
2015-11-12 18:21:27 -0600

    Add ability to create comments

--- dbc_researcher/app/controllers/comments_controller.rb
+++ dbc_researcher/app/controllers/comments_controller.rb
@@ -4,6 +4,20 @@ class CommentsController < ApplicationController
   end
 
   def create
-    @comment =
+    p params
+    @comment = Proposal.find_by(id: params[:proposal_id]).comments.create(comment_params.merge({user_id: current_user.id}))
+    # @comment = current_user.comments.create(comment_params)
+
+    if @comment.valid?
+      p "it saved"
+    else
+      p "it didn't save"
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:comment_text)
   end
 end
