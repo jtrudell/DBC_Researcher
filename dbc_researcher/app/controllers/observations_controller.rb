d059bbd43c3e844cf261060cbe4f140662ff6706
Kyle Dolezal <email": null>
2015-11-13 12:29:42 -0600

    Observation creation redirects back to experiment

--- dbc_researcher/app/controllers/observations_controller.rb
+++ dbc_researcher/app/controllers/observations_controller.rb
@@ -23,8 +23,9 @@ class ObservationsController < ApplicationController
     @observation.assign_attributes(observation_params)
 
     if @observation.save
-      redirect_to proposal_experiment_observations_path
+      redirect_to proposal_experiment_path(params[:proposal_id], params[:experiment_id])
     else
+      flash[:error] = "Error, please enter an observation"
       redirect_to edit_proposal_experiment_observation_path
     end
   end
@@ -33,9 +34,11 @@ class ObservationsController < ApplicationController
     @observation = current_user.observations.create(observation_text: observation_params[:observation_text], experiment_id: params[:experiment_id])
 
     if @observation.valid?
-      redirect_to proposal_experiment_observations_path
+      redirect_to proposal_experiment_path(params[:proposal_id], params[:experiment_id])
+
     else
-      redirect_to new_proposal_experiment_observation_path
+      flash[:error] = "Error, please enter an observation"
+      redirect_to edit_proposal_experiment_observation_path
     end
   end
 
