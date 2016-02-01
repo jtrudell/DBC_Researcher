f74297886d4e475d33f4d018e31f824d502e513f
dclorena11 <dclorena11@gmail.com>
2016-02-01 09:41:26 -0600

    remove unnecessary code

--- dbc_researcher/app/controllers/comments_controller.rb
+++ dbc_researcher/app/controllers/comments_controller.rb
@@ -4,26 +4,20 @@ class CommentsController < ApplicationController
   end
 
   def create
-    # @comment = Proposal.find_by(id: params[:proposal_id]).comments.create(comment_params.merge({user_id: current_user.id}))
-
     @observation = Observation.find_by(id: params[:observation_id])
     @experiment = Experiment.find_by(id: params[:experiment_id])
     @proposal = Proposal.find_by(id: params[:proposal_id])
 
     if @observation
       @comment = @observation.comments.create(comment_params.merge({user_id: current_user.id}))
-      # render json:  {comment_text: @comment.comment_text, name: @comment.user.name}
       redirect_to proposal_experiment_observation_path(@proposal, @experiment, @observation)
 
     elsif @experiment
       @comment = @experiment.comments.create(comment_params.merge({user_id: current_user.id}))
-      # render json:  {comment_text: @comment.comment_text, name: @comment.user.name}
       redirect_to proposal_experiment_path(@proposal, @experiment)
 
     elsif @proposal
-      p params
       @comment = @proposal.comments.create(comment_params.merge({user_id: current_user.id}))
-      # render json:  {comment_text: @comment.comment_text, name: @comment.user.name}
       redirect_to proposal_path(@proposal)
     end
 
